c8b192563b25b82454dd476790c6de1b79a4d8a2
Scott Wardle <swardle@gmail.com>
2021-12-18 19:45:51 -0800

    Add support for ReCAPTCHA to stop the email spam

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"runtime"
 	"time"
@@ -18,13 +20,24 @@ import (
 	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
 )
 
-func addSecretsToEnv() error {
-	// Create the client.
-	ctx := context.Background()
-	client, err := secretmanager.NewClient(ctx)
-	if err != nil {
-		return fmt.Errorf("failed to create secretmanager client: %v", err)
+func addReCaptchaSecretsToEnv(ctx context.Context, client *secretmanager.Client) error {
+	// Build the request.
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: "projects/694671698910/secrets/reCAPTCHA/versions/latest",
 	}
+
+	// Call the API.
+	result, err := client.AccessSecretVersion(ctx, req)
+	if err != nil {
+		fmt.Printf("failed to access secret version: %v", err)
+		return fmt.Errorf("failed to access secret version: %v", err)
+	}
+
+	os.Setenv("RECAPTCHA_API_KEY", string(result.Payload.Data))
+	return nil
+}
+
+func addSendGridSecretsToEnv(ctx context.Context, client *secretmanager.Client) error {
 
 	// Build the request.
 	req := &secretmanagerpb.AccessSecretVersionRequest{
@@ -42,11 +55,46 @@ func addSecretsToEnv() error {
 	return nil
 }
 
+// GoogleRecaptchaResponse ...
+type GoogleRecaptchaResponse struct {
+	Success            bool     `json:"success"`
+	ChallengeTimestamp string   `json:"challenge_ts"`
+	Hostname           string   `json:"hostname"`
+	ErrorCodes         []string `json:"error-codes"`
+}
+
+// This will handle the reCAPTCHA verification between your server to Google's server
+func validateReCAPTCHA(recaptchaResponse string) (bool, error) {
+
+	// Check this URL verification details from Google
+	// https://developers.google.com/recaptcha/docs/verify
+	req, err := http.PostForm("https://www.google.com/recaptcha/api/siteverify", url.Values{
+		"secret":   {os.Getenv("RECAPTCHA_API_KEY")},
+		"response": {recaptchaResponse},
+	})
+	if err != nil { // Handle error from HTTP POST to Google reCAPTCHA verify server
+		return false, err
+	}
+	defer req.Body.Close()
+	body, err := ioutil.ReadAll(req.Body) // Read the response from Google
+	if err != nil {
+		return false, err
+	}
+
+	var googleResponse GoogleRecaptchaResponse
+	err = json.Unmarshal(body, &googleResponse) // Parse the JSON response from Google
+	if err != nil {
+		return false, err
+	}
+	return googleResponse.Success, nil
+}
+
 type contactForm struct {
-	Name    string `json:"name"`
-	Email   string `json:"email"`
-	Subject string `json:"subject"`
-	Message string `json:"message"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Subject   string `json:"subject"`
+	Message   string `json:"message"`
+	ReCaptcha string `json:"g-recaptcha-response"`
 }
 
 func sendHandle(rw http.ResponseWriter, req *http.Request) {
@@ -65,6 +113,14 @@ func sendHandle(rw http.ResponseWriter, req *http.Request) {
 	c.Email = req.FormValue("email")
 	c.Subject = req.FormValue("subject")
 	c.Message = req.FormValue("message")
+	c.ReCaptcha = req.FormValue("g-recaptcha-response")
+
+	result, err := validateReCAPTCHA(c.ReCaptcha)
+	if err != nil || !result {
+		errstr := fmt.Sprintf("error = %v result = %v", err, result)
+		http.Error(rw, "please hit I am not robot button\n or = "+errstr, 500)
+		return
+	}
 
 	// this function returns the present time
 	now := time.Now()
@@ -104,6 +160,30 @@ func sendHandle(rw http.ResponseWriter, req *http.Request) {
 	http.Redirect(rw, req, "thanks.html", http.StatusSeeOther)
 }
 
+func AddSecretToEnv() {
+	// Create the client.
+	// this will fail on windows
+	// so manually add these envioment
+	// vars to your envioment
+	ctx := context.Background()
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		log.Println(fmt.Sprintf("failed to create secretmanager client: %v", err))
+		return
+	}
+
+	// using "GOOGLE_APPLICATION_CREDENTIALS" on windows
+	apikey := os.Getenv("SENDGRID_API_KEY")
+	if apikey == "" {
+		addSendGridSecretsToEnv(ctx, client)
+	}
+
+	apikey = os.Getenv("RECAPTCHA_API_KEY")
+	if apikey == "" {
+		addReCaptchaSecretsToEnv(ctx, client)
+	}
+}
+
 func main() {
 
 	isWin := false
@@ -115,11 +195,6 @@ func main() {
 	// Prefix text prevents the message from being parsed as JSON.
 	// A timestamp is added when shipping logs to Cloud Logging.
 	log.SetFlags(0)
-	// using "GOOGLE_APPLICATION_CREDENTIALS" on windows
-	apikey := os.Getenv("SENDGRID_API_KEY")
-	if apikey == "" {
-		addSecretsToEnv()
-	}
 
 	port := os.Getenv("PORT")
 	if port == "" {
